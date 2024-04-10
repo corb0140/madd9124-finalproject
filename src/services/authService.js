4eda57f411a655e4ac75d5458ce779cd5dd693a7
Mark Corbin <corb0140@algonquinlive.com>
2024-04-09 23:19:07 -0400

    testing render

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,8 @@
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const { UnauthenticatedError } = require("../middlewares/errors");
 
 const generateToken = (_id) =>
   jwt.sign({ id: _id.toString() }, process.env.JWT_SECRET, {
@@ -6,4 +10,32 @@ const generateToken = (_id) =>
     expiresIn: "14 days",
   });
 
-module.exports = { generateToken };
+const login = async (username, password) => {
+  const foundUser = await User.findOne({ username });
+
+  if (!foundUser) {
+    throw new UnauthenticatedError("Invalid input");
+  }
+
+  const isValidPassword = bcrypt.compareSync(password, foundUser.password);
+  if (!isValidPassword) {
+    throw new UnauthenticatedError("Invalid input");
+  }
+  return generateToken(foundUser._id);
+};
+
+const register = async (username, password) => {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = new User({
+    username,
+    password: hashedPassword,
+  });
+  const savedUser = await user.save();
+  return generateToken(savedUser._id);
+};
+
+module.exports = {
+  generateToken,
+  login,
+  register,
+};
